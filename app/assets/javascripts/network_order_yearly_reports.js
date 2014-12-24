7140d04436181951e20a3e3d1d78c93a0e35f8e5
jjhlzn <jinjunhang@hotmail.com>
2014-12-24 15:16:18 +0800

    加入游客使用手机的运营商数据对比分析

--- app/assets/javascripts/network_order_yearly_reports.js
+++ app/assets/javascripts/network_order_yearly_reports.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-    /*
+
     new PieReportChart({
         id: 'production_compare_pie',
         name: '门票, 酒店, 套餐比例',
         url: '/network_order_yearly_reports.json',
         menus: [{type: 'year'}, {type: 'indicator'}]
-    }).send_request();*/
+    }).send_request();
+
     function response_deal_fuction(respJSON) {
         function pie_draw_table(id, datasets_src, topn) {
             var table_id = id+'_table';
