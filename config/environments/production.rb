c8811ff542117b2fb9d7d2d71487e1e29cf7e52a
jjhlzn <jinjunhang@hotmail.com>
2014-12-31 09:37:35 +0800

    deploy cofig

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.mssql_host = 'e.hengdianworld.com'
-  config.mssql_ticket_server = ''
+  config.mssql_host = '192.168.1.110'
+  config.mssql_ticket_server = 'TicketServer'
   config.mssql_username = 'sa'
   config.mssql_password = 'hdapp@)!@'
 
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
